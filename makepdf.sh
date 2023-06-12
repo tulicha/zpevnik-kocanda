553afa86a4beb96ed7ee3dcc61ad262c5364c540
tulicha <slavion7@fit.cvut.cz>
2023-06-12 17:38:05 +0200

    Make folder for pisnicky and make makescript

---
+++ makepdf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Make sure this script has right to write, read and execute!
+
+echo "Making pdf..."
+echo "----------------------"
+ls pisnicky > seznam.tex
+sed -i -e 's&^&\\input pisnicky/&' seznam.tex
+optex zpevnik
+optex zpevnik
+echo "----------------------"
+echo "Done!"
